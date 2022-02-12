d407a5c7f4f3545fa175951fd7baf8721aa81942
Gilles Lengy <hello@gilleslengy.dev>
2022-02-12 22:40:33 +0100

    The title, user portrait and/ name can now be displayed on the vimeo video homepage

--- homepage/homepage-vimeo.php
+++ homepage/homepage-vimeo.php
@@ -1,9 +1,15 @@
-<?php $homepage_vimeo_video_id = get_theme_option('homepage_vimeo_video_id'); ?>
-<?php $homepage_vimeo_video_color = get_theme_option('homepage_vimeo_video_color'); ?>
+<?php
+$homepage_vimeo_video_id = get_theme_option('homepage_vimeo_video_id');
+$homepage_vimeo_video_color = get_theme_option('homepage_vimeo_video_color');
+$homepage_vimeo_video_title = get_theme_option('homepage_vimeo_video_title');
+$homepage_vimeo_video_portrait = get_theme_option('homepage_vimeo_video_portrait');
+$homepage_vimeo_video_byline = get_theme_option('homepage_vimeo_video_byline');
+?>
+
 <div id="homepage-vimeo">
     <div class="responsive-embed widescreen">
         <iframe
-                src="https://player.vimeo.com/video/<?php echo $homepage_vimeo_video_id; ?>?&portrait=0&title=no&byline=0<?php if ($homepage_vimeo_video_color): echo "&color=$homepage_vimeo_video_color"; endif; ?>"
+                src="https://player.vimeo.com/video/<?php echo $homepage_vimeo_video_id; ?>?&portrait=<?php echo $homepage_vimeo_video_portrait; ?>&title=<?php echo $homepage_vimeo_video_title; ?>&byline=<?php echo $homepage_vimeo_video_byline; ?><?php if ($homepage_vimeo_video_color): echo "&color=$homepage_vimeo_video_color"; endif; ?>"
                 webkitallowfullscreen mozallowfullscreen allowfullscreen>
         </iframe>
     </div>
