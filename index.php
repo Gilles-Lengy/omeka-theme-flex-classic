95e6837e57ead448eea32e2189ffa9991e5720a1
Gilles Lengy <hello@gilleslengy.dev>
2022-03-21 15:48:39 +0100

    featured-records common template used in index.php

--- index.php
+++ index.php
@@ -10,36 +10,8 @@ endif;
 
 <?php echo common('homepage-youtube', array(), 'homepage'); ?>
 
-<div class="featured-records">
-    <?php $mainFeaturedRecordType = (get_theme_option('home_main_featured') !== null) ? get_theme_option('home_main_featured') : 'item'; ?>
-    <!-- Featured Collection -->
-    <div class="main featured">
-        <?php if (($mainFeaturedRecordType == 'exhibit') && !plugin_is_active('ExhibitBuilder')): ?>
-            <?php echo __('Exhibit Builder is not installed.'); ?>
-        <?php else: ?>
-            <?php echo foundation_random_featured_records_html($mainFeaturedRecordType); ?>
-        <?php endif; ?>
-    </div><!-- end featured collection -->
+<?php echo common('featured-records'); ?>
 
-
-    <div class="supporting featured">
-        <?php $secondFeaturedRecordType = (get_theme_option('home_second_featured') !== null) ? get_theme_option('home_second_featured') : 'item'; ?>
-        <!-- Featured Collection -->
-        <?php if (($secondFeaturedRecordType == 'exhibit') && !plugin_is_active('ExhibitBuilder')): ?>
-            <?php echo __('Exhibit Builder is not installed.'); ?>
-        <?php else: ?>
-            <?php echo foundation_random_featured_records_html($secondFeaturedRecordType); ?>
-        <?php endif; ?>
-
-        <?php $thirdFeaturedRecordType = (get_theme_option('home_third_featured') !== null) ? get_theme_option('home_third_featured') : 'item'; ?>
-        <!-- Featured Collection -->
-        <?php if (($thirdFeaturedRecordType == 'exhibit') && !plugin_is_active('ExhibitBuilder')): ?>
-            <?php echo __('Exhibit Builder is not installed.'); ?>
-        <?php else: ?>
-            <?php echo foundation_random_featured_records_html($thirdFeaturedRecordType); ?>
-        <?php endif; ?>
-    </div>
-</div>
 
 <?php if (get_theme_option('Homepage Text')): ?>
     <?php $introBg = (get_theme_option('Homepage Intro Background') ? 'style="background-image:url(' . foundation_homepage_intro_background() . ')"' : ''); ?>
