24e25a29c9ee63ba4559e04245a79b561eecc354
Gilles Lengy <hello@gilleslengy.dev>
2022-03-21 18:00:30 +0100

    homepage_elements use in index.php to follow the order of the element.

--- index.php
+++ index.php
@@ -6,17 +6,41 @@ endif;
 
 <?php echo head(array('bodyid' => 'home')); ?>
 
-<?php echo common('homepage-vimeo', array(), 'homepage'); ?>
+<?php
+$homepageElementsStr = get_theme_option('homepage_elements');
 
-<?php echo common('homepage-youtube', array(), 'homepage'); ?>
+if ($homepageElementsStr):
 
-<?php echo common('homepage-neatline-time-timeline', array(), 'homepage'); ?>
+    $homepageElementsArray = explode('|', $homepageElementsStr);
 
-<?php echo common('homepage-text', array(), 'homepage'); ?>
+    foreach ($homepageElementsArray as $element):
+        switch ($element) {
+            case 'vimeo':
+                echo common('homepage-vimeo', array(), 'homepage');
+                break;
+            case 'youtube':
+                echo common('homepage-youtube', array(), 'homepage');
+                break;
+            case 'timeline':
+                echo common('homepage-neatline-time-timeline', array(), 'homepage');
+                break;
+            case 'text':
+                echo common('homepage-text', array(), 'homepage');
+                break;
+            case 'featured':
+                echo common('featured-records');
+                break;
+            case 'items':
+                echo common('recent-items');
+                break;
+        }
+    endforeach;
 
-<?php echo common('featured-records'); ?>
-
-<?php echo common('recent-items'); ?>
+else:
+    // Will use the translator system later or sooner !!!!
+    ?>
+    <p>Configure the elements you want in theme configuration</p>
+<?php endif; ?>
 
 <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
